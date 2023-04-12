69b36c2f474189ebe8e4e2f0543bc4da4894380f
CarlyGallagher <jagllaghercarly3@yahoo.com>
2023-04-11 20:18:27 -0700

    added signup handlebar code

--- controllers/api/userRoutes.js
+++ controllers/api/userRoutes.js
@@ -6,7 +6,6 @@ router.post("/", async (req, res) => {
   try {
     const dbUserData = await User.create({
       username: req.body.username,
-      email: req.body.email,
       password: req.body.password,
     });
 
