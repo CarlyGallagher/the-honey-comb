942d9a4e8db4cb557aa8e266984c44c0229d1e8b
Samantha Orozco <orozcoxs@gmail.com>
2023-04-13 19:15:03 -0700

    login code added

---
+++ public/javascript/login.js
@@ -0,0 +1,53 @@
+async function loginFormHandler(event) {
+    event.preventDefault();
+
+    const username = document.querySelector('#username-login').value.trim();
+    const password = document.querySelector('#password-login').value.trim();
+
+    if (username && password) {
+        const response = await fetch('/api/users/login', {
+            method: 'post',
+            body: JSON.stringify({
+                username,
+                password
+            }),
+            headers: { 'Content-Type': 'application/json' }
+        });
+
+        if (response.ok) {
+            document.location.replace('/');
+        } else {
+            alert(response.statusText);
+        }
+    }
+}
+
+app.post('/auth', function(request, response) {
+	
+	let username = request.body.username;
+	let password = request.body.password;
+	
+	if (username && password) {
+		
+		connection.query('SELECT * FROM accounts WHERE username = ? AND password = ?', [username, password], function(error, results, fields) {
+			
+			if (error) throw error;
+			
+			if (results.length > 0) {
+				
+				request.session.loggedin = true;
+				request.session.username = username;
+				
+				response.redirect('/home');
+			} else {
+				response.send('Incorrect Username and/or Password!');
+			}			
+			response.end();
+		});
+	} else {
+		response.send('Please enter Username and Password!');
+		response.end();
+	}
+});
+
+document.querySelector('#login-form').addEventListener('submit', loginFormHandler);
